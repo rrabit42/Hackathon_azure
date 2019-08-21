b270c420dd5e398782e1ce18ed8a2218a02f760c
rrabit42 <gegiraffe@naver.com>
2019-08-21 21:24:14 +0900

    requrements와 settings 분리

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: render(request, 'root.html'), name='root'),
 ]
 
 # 장고는 개발서버에서 media 파일 지원하지 않아서 이렇게 해줌
